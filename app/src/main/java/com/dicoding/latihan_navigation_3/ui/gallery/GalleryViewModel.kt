181f311032f60b9495a3229d08f148f3139f11ba
Ubean <ubeannn@gmail.com>
2021-11-22 14:51:08 +0700

    Initial commit

---
+++ app/src/main/java/com/dicoding/latihan_navigation_3/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.dicoding.latihan_navigation_3.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
